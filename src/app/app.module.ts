aa651ae2938640cb2070e7209200fbbc14ae16b3
Silverado <uninstallgame4444@gmail.com>
2019-10-05 19:47:39 +0200

    directive invalid input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { DateRangeDirective } from "./directive/date-range.directive";
+import { FormDirective } from "./directive/focusInvalidinput.directive";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { FormsModule } from "@angular/forms";
@@ -10,7 +11,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [AppComponent, DateRangeDirective],
+  declarations: [AppComponent, DateRangeDirective, FormDirective],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
